b574af55e45a4d2d9f8970937db25fa5096b52c3
Chris Fanucchi <admin@knightskeep.com>
2022-08-29 16:19:44 -0700

    Listener working and updating items clicked/selected in objects

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 document.querySelector('aside').classList.add('hidden');
 
 let products = [];
-let votingRounds = 25;
+let votingRounds = 1;
+let numProductsDisplayed = 3;
 let productImages = [
   'bag.jpg',
   'boots.jpg',
@@ -47,3 +48,72 @@ for (let product of productImages) {
     prodImgPath: `./img/${product}`,
   });
 }
+
+// Loop to the maximum of voting times allowed by votingRounds
+for (let i = 0; i < votingRounds; i++) {
+  let uniqueProducts = [];
+
+  // Loop until the numProductsDisplayed number of unique items are chosen for display
+  while (uniqueProducts.length < numProductsDisplayed) {
+    let product = getRandomProducts(products);
+
+    // Add product to uniqueProducts array if not already chosen
+    if (!uniqueProducts.includes(product)) {
+      uniqueProducts.push(product);
+      product.timesShown++;
+    }
+  }
+  displayProducts(uniqueProducts);
+}
+
+// Select a product randomly from an array of products
+function getRandomProducts(productsArray) {
+  let randomNumber = Math.floor(Math.random() * productsArray.length);
+  let productChosen = productsArray[randomNumber];
+  return productChosen;
+}
+
+// Displays product images and names to the DOM
+function displayProducts(arrayToDisplay) {
+  let pickImagesEl = document.getElementById('pick-images');
+  let imageNumber = 0;
+
+  for (let product of arrayToDisplay) {
+    let sectionEl = document.createElement('section');
+    sectionEl.classList.add(`image-section-${imageNumber}`);
+    pickImagesEl.appendChild(sectionEl);
+    let imgEl = document.createElement('img');
+    imgEl.alt = product.prodName;
+    imgEl.classList.add(`image-${imageNumber}`);
+    imgEl.src = product.prodImgPath;
+    sectionEl.appendChild(imgEl);
+    let pEl = document.createElement('p');
+    pEl.textContent = product.prodName;
+    sectionEl.appendChild(pEl);
+    imageNumber++; // Increment variable to create successive class names
+  }
+
+  // Get selections from user and denote in product object
+  let clickedElement;
+  document
+    .querySelector('#pick-images')
+    .addEventListener('click', function (event) {
+      clickedElement = event.target.alt;
+      if (typeof clickedElement == 'string') {
+        // ***** Process clicked item
+        updateSelectedProducts(clickedElement);
+        console.log(products);
+      }
+    });
+}
+
+function updateSelectedProducts(selectedProduct) {
+  for (let i = 0; i < products.length; i++) {
+    if (selectedProduct === products[i].prodName) {
+      products[i].timesSelected++;
+      break;
+    }
+  }
+}
+
+console.log(products);
